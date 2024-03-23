9134a6bb45b1280b1e64f92d6d3d223eaf378b8e
Niels <n.koster@portavita.eu>
2024-03-23 23:24:52 +0100

    Allow web page as argument, default to https

--- main.go
+++ main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"fmt"
 	webview "github.com/webview/webview_go"
+	"os"
 )
 
 var err error
 
 func main() {
+	// get the first command line argument
+	// if it is not empty, use it as the URL
+	url := ""
+	if len(os.Args) > 1 {
+		url = os.Args[1]
+	}
+	fmt.Println("URL:", url)
+	// check if the URL starts with "http://", if not, add "https://"
+	if url != "" {
+		url = testUrl(url)
+	}
 	w := webview.New(true)
 	defer w.Destroy()
 	w.SetTitle("Browser")
 	w.SetSize(1200, 800, webview.HintNone)
-	w.SetHtml("Thanks for using Browser!")
-	w.Navigate("https://go.dev")
 	err = w.Bind("updateTitle", func(title string) {
 		fmt.Println("Received page title:", title)
 		w.SetTitle(title)
@@ -22,11 +32,17 @@ func main() {
 		fmt.Println("Error binding function:", err)
 	}
 	err = w.Bind("navigate", func(url string) {
+		url = testUrl(url)
 		fmt.Println("Navigate to:", url)
 		w.Navigate(url)
 	})
 	if err != nil {
 		fmt.Println("Error binding function:", err)
+	}
+	if url != "" {
+		w.Navigate(url)
+	} else {
+		w.SetHtml("Thanks for using Browser!")
 	}
 	w.Init(`
         document.addEventListener("DOMContentLoaded", function() {
@@ -45,7 +61,7 @@ func main() {
             }
 		});
 		function promptForURL() {
-            var url = prompt("Open page:", "https://");
+            var url = prompt("Open page:", "");
 			if (url) {
                 window.navigate(url);
             }
@@ -53,4 +69,13 @@ func main() {
 	`)
 
 	w.Run()
+}
+
+func testUrl(url string) string {
+	if len(url) < 7 {
+		url = "https://" + url
+	} else if url[:7] != "http://" {
+		url = "https://" + url
+	}
+	return url
 }
