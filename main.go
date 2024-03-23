c47ac80eaccf3a7f62b35a6cdd052983153a825c
Niels <n.koster@portavita.eu>
2024-03-23 21:41:05 +0100

    Add URL input dialog

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	webview "github.com/webview/webview_go"
 )
 
+var err error
+
 func main() {
 	w := webview.New(true)
 	defer w.Destroy()
@@ -12,27 +14,43 @@ func main() {
 	w.SetSize(1200, 800, webview.HintNone)
 	w.SetHtml("Thanks for using Browser!")
 	w.Navigate("https://go.dev")
-	err := w.Bind("updateTitle", func(title string) {
+	err = w.Bind("updateTitle", func(title string) {
 		fmt.Println("Received page title:", title)
 		w.SetTitle(title)
 	})
 	if err != nil {
 		fmt.Println("Error binding function:", err)
 	}
+	err = w.Bind("navigate", func(url string) {
+		fmt.Println("Navigate to:", url)
+		w.Navigate(url)
+	})
+	if err != nil {
+		fmt.Println("Error binding function:", err)
+	}
 	w.Init(`
-		document.addEventListener("DOMContentLoaded", function() {
+        document.addEventListener("DOMContentLoaded", function() {
 			if (typeof window.updateTitle === "function") {
     			window.updateTitle(document.title)
-				.then(function(result) {
-        			console.log("Title updated.", result);
-    			})
 				.catch(function(error) {
         			console.error("Title update failed.", error);
     			});
 			} else {
-    			console.error("updateTitle function was not defined.");
+              console.error("updateTitle undefined.");
 			}
 		});
+		document.addEventListener("keydown", function(event) {
+            if (event.ctrlKey && event.altKey && event.key === "u") {
+                promptForURL();
+            }
+		});
+		function promptForURL() {
+            var url = prompt("Open page:", "https://");
+			if (url) {
+                window.navigate(url);
+            }
+		}
 	`)
+
 	w.Run()
 }
